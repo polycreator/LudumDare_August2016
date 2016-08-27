6b69903f05494ce5ac8603d02ea3d22d2378377f
Brian Miller <brian@polycreator.com>
2016-08-27 17:27:49 +0200

    First Blood!

---
+++ Assets/animation_test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animation_test : MonoBehaviour {
+
+    public AnimationClip clip;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
