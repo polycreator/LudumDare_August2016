1419e5474aa9954df1bf23075b8a88ee2f6f8d20
Arthur Langereis <puurtuur@me.com>
2016-08-28 21:10:54 +0200

    camera buttons

---
+++ Assets/Scripts/PanelButton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum ButtonState {
+	Idle,
+	Disabled,
+	Pressing
+}
+
+public class PanelButton : MonoBehaviour {
+
+	private ButtonState state_ = ButtonState.Idle;
+	private bool enabled_ = true;
+	private float startTime_ = 0;
+	private Vector3 originalPos_ ;
+
+	const float PressTime = 1.0f;
+	const float Depression = 0.015f;
+
+	void Start() {
+		originalPos_ = gameObject.transform.localPosition;
+	}
+
+	float ButtonPosition(float t) {
+		return Mathf.Sin(Mathf.PI * t);
+	}
+	
+	void Update() {
+		if (state_ == ButtonState.Pressing) {
+			var timeRatio = Mathf.Clamp01((Time.time - startTime_) / PressTime);
+			var depress = ButtonPosition(timeRatio);
+			var newPos = originalPos_;
+			newPos -= gameObject.transform.rotation * new Vector3(0, depress * Depression, 0);
+			gameObject.transform.localPosition = newPos;
+
+			if (timeRatio == 1.0) {
+				state_ = enabled_ ? ButtonState.Idle : ButtonState.Disabled;
+			}
+		}
+	}
+
+	public void Press() {
+		if (state_ != ButtonState.Idle) {
+			return;
+		}
+
+		state_ = ButtonState.Pressing;
+		startTime_ = Time.time;
+	}
+
+	public void SetEnabled(bool newEnabled) {
+		enabled_ = newEnabled;
+		if (state_ != ButtonState.Pressing) {
+			state_ = enabled_ ? ButtonState.Idle : ButtonState.Disabled;
+		}
+	}
+}
---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Input.GetMouseButtonDown(0)) {
+			if (Physics.Raycast(ray, out hit)) {
+				var button = hit.collider.gameObject.GetComponent<PanelButton>();
+				button.Press();
+			}
+		}
+	}
+}
