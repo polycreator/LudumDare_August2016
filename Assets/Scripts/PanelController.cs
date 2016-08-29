19829d02d124fe0fe3c1d8c7b6646e7018f210e5
Arthur Langereis <puurtuur@me.com>
2016-08-29 20:21:17 +0200

    trigger traps

--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PanelController : MonoBehaviour {
 
+	private string cameraIndex_ = "4";
+
 	// Use this for initialization
 	void Start () {
 
@@ -19,10 +21,28 @@ public class PanelController : MonoBehaviour {
 				if (button && button.Press()) {
 					// camera switchers
 					if (button.name.StartsWith("CamButton")) {
-						var buttonIndexStr = button.name.Substring("CamButton".Length);
+						cameraIndex_ = button.name.Substring("CamButton".Length);
 						var allCams = GameObject.FindGameObjectsWithTag("Security Camera");
 						foreach (var cam in allCams) {
-							cam.GetComponent<Camera>().enabled = cam.name == "camera_0" + buttonIndexStr;
+							cam.GetComponent<Camera>().enabled = cam.name == "camera_0" + cameraIndex_;
+						}
+					}
+					else if (button.name.StartsWith("TrapButton")) {
+						// trap activators
+						var curCam = GameObject.Find("camera_0" + cameraIndex_);
+						if (curCam) {
+							print("have cam " + cameraIndex_);
+							var trapCtl = curCam.GetComponent<CameraTrap_Controller>();
+							if (trapCtl) {
+								print("have trapCtl: "+ button.name);
+								var trapIx = int.Parse(button.name.Substring("TrapButton".Length));
+								print(trapIx);
+								if (trapCtl.trapController.Length > trapIx) {
+									print("trapIx OK");
+									print(trapCtl.trapController[trapIx].gameObject.name);
+									trapCtl.trapController[trapIx].ActivateTrap();
+								}
+							}
 						}
 					}
 				}
