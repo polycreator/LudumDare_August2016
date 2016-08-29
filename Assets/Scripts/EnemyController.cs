7b663647a3b2a36d2f3d3f44a4c9b5910ced765d
Arthur Langereis <puurtuur@me.com>
2016-08-30 00:31:35 +0200

    all waypoints

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -18,7 +18,8 @@ public class EnemyController : MonoBehaviour {
 	}
 
 	private GameObject pickRandomSpawnPoint() {
-		return spawnPoints[0].gameObject;
+		int spix = Mathf.RoundToInt(Random.Range(0, (float)spawnPoints.Length - 1));
+		return spawnPoints[spix].gameObject;
 	}
 
 	private void spawnEnemyAt(GameObject spawnPoint) {
