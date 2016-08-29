4faca608f20dafbf931aef4fc40856bf04b20369
Brian Miller <brian@polycreator.com>
2016-08-30 01:12:38 +0200

    quick fixes and mutli scene loading

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevelAdditive("level01");
+        Application.LoadLevelAdditive("control_room");
+        Application.LoadLevelAdditive("ControlData");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
